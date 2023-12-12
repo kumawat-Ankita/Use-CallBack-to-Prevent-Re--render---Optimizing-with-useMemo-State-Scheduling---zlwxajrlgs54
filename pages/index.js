160b07fe4d6377a2c1954c8ab6127e6dbccac8b3
Ankita <ankitakumawat111@gmail.com>
2023-12-12 18:18:00 +0530

    Update code

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,20 @@ export default function Home() {
     const [items, setItems] = useState([]);
     const [message, setMessage] = useState('');
 
-    
+    const addItem = useCallback(() => {
+        const newItemName = `Item ${items.length + 1}`;
+        setItems(prevItems => [...prevItems, newItemName]);
+        setMessage('Item added successfully!');
+
+        setTimeout(() => {
+            setMessage('');
+        }, 3000);
+    }, [items.length]);
+
 
     return (
         <div>
-            <button >Add Item</button>
+            <button onClick={addItem}>Add Item</button>
             <ItemList items={items} />
             {message && <p id='message'>{message}</p>}
         </div>
